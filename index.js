ef03e49f69d60deacc51adf518af9864b32c2176
RositaguiUP <0225352@up.edu.mx>
2020-11-07 06:35:19 -0600

    funciona

--- index.js
+++ index.js
@@ -5,19 +5,13 @@ const Movie = require('./src/models/movie'); // importa el esquema
 const methodOverride = require('method-override');
 require('./src/services/connection'); // importa el archivo de conexión
 
-//const bodyParser = require('body-parser');
-//const path = require('path');
-//app.set('views', path.join(__dirname, 'views'));
-//app.use(express.static(path.join(__dirname)));
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-//app.use(express.json());
 
 app.get('/', async(req, res) => {
-    const movies = await Movie.find().sort({ title: 'desc' });
+    const movies = await Movie.find();
     res.render('index', { movies: movies });
 });
 
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,25 +1,36 @@
-const express = require('express')
-const Movie = require('./../models/movie')
-const router = express.Router()
+const express = require('express');
+const Movie = require('./../models/movie');
+const router = express.Router();
 
 router.get('/post', (req, res) => {
     res.render('add', { movie: new Movie() });
 });
 
+
 router.get('/edit/:id', async(req, res) => {
-    res.sendFile(__dirname + '/html/form.html');
+    const movie = await Movie.findById(req.params.id);
+    res.render('edit', { movie: movie });
 });
+/*
+router.get('/:id', async(req, res) => {
+    res.render('edit', { movie: new Movie() });
+});*/
 
 router.get('/:title', async(req, res) => {
     const movie = await Movie.findOne({ title: req.params.title });
     if (movie == null) res.redirect('/');
     res.render('details', { movie: movie });
-})
+});
 
 router.post('/post', (req, res, next) => {
-    //req.movie = new Movie();
+    req.movie = new Movie();
     next();
 }, saveMovieAndRedirect('add'));
+
+router.put('/:id', async(req, res, next) => {
+    req.movie = await Movie.findById(req.params.id);
+    next();
+}, saveMovieAndRedirect('edit'));
 
 function saveMovieAndRedirect(path) {
     return async(req, res) => {
@@ -37,28 +48,26 @@ function saveMovieAndRedirect(path) {
         const dur = req.body.inputDuration;
         const comp = req.body.inputCompany;
 
-        const movie = new Movie({
-            title: tit,
-            director: dir,
-            description: desc,
-            rating: rat,
-            genre: genr,
-            language: lang,
-            producer: prod,
-            writer: wrt,
-            releaseDate: relDt,
-            streamingRelease: strRel,
-            earnings: earn,
-            duration: dur,
-            company: comp
-        });
+        let movie = req.movie;
+        movie.title = tit;
+        movie.director = dir;
+        movie.description = desc;
+        movie.rating = rat;
+        movie.genre = genr;
+        movie.language = lang;
+        movie.producer = prod;
+        movie.writer = wrt;
+        movie.releaseDate = relDt;
+        movie.streamingRelease = strRel;
+        movie.earnings = earn;
+        movie.duration = dur;
+        movie.company = comp;
 
         try {
             movie = await movie.save();
-            //res.redirect(`/movies/${movie.tit}`);
-            res.render(`${path}`, { movie: movie });
+            res.redirect(`/movies/${movie.tit}`);
         } catch (e) {
-            res.render(`${path}`, { movie: movie });
+            res.render(`movies/${path}`, { movie: movie });
         }
     }
 }
